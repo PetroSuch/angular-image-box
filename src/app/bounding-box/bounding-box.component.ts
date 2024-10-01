2261054ab51d6fc57b4e07831b320831444035db
Petro Sych <petro.sych@careerteacherhub.com>
2024-10-01 15:07:30 +0300

    Init project

---
+++ src/app/bounding-box/bounding-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bounding-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './bounding-box.component.html',
+  styleUrl: './bounding-box.component.scss'
+})
+export class BoundingBoxComponent {
+
+}
